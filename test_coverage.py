6987cc34f899e45c9d649f2c74702f1c0b402881
Palladain <34582887+Palladain@users.noreply.github.com>
2023-11-22 13:01:57 +0300

    Update test_coverage.py

--- test_coverage.py
+++ test_coverage.py
@@ -1,11 +1,7 @@
 import json
 
-def check_info_coverage_json(filename='coverage.json'):
+def test_info_coverage_json(filename='coverage.json'):
     file = open(filename, "r")
  
     data = json.loads(file.read())
     assert data['totals']['percent_covered'] == 100
-
-if __name__ == "__main__":
-
-    check_info_coverage_json()
